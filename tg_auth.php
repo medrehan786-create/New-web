03f3f8f4dec0f3371f959d6f9cd60752de852858
medrehan786-create <medrehan786@gmail.com>
2025-09-27 21:25:45 +0530

    Update tg_auth.php

--- tg_auth.php
+++ tg_auth.php
@@ -1,35 +1,33 @@
 <?php
-require 'config.php';
+require __DIR__.'/config.php';
 header('Content-Type: application/json');
 
-// Telegram sends JSON: { "initData": "hash=...&id=..." }
+// Get JSON POST from JS
 $data = json_decode(file_get_contents('php://input'), true);
-
-if (empty($data['initData'])) {
+if(empty($data['initData'])){
     echo json_encode(['ok'=>false,'error'=>'No Telegram data received']);
     exit;
 }
 
-// Parse initData string into array
 parse_str($data['initData'], $tgUserData);
 
-if (!verifyTelegramAuth($tgUserData)) {
+if(!verifyTelegramAuth($tgUserData)){
     echo json_encode(['ok'=>false,'error'=>'Invalid Telegram signature']);
     exit;
 }
 
 // Upsert user
 $user = upsert_user_from_tg([
-    'id' => $tgUserData['id'],
-    'username' => $tgUserData['username'] ?? null,
-    'first_name' => $tgUserData['first_name'] ?? null,
-    'last_name' => $tgUserData['last_name'] ?? null,
-    'photo_url' => $tgUserData['photo_url'] ?? null,
-    'auth_date' => $tgUserData['auth_date'] ?? time()
+    'id'=>$tgUserData['id'],
+    'username'=>$tgUserData['username']??null,
+    'first_name'=>$tgUserData['first_name']??null,
+    'last_name'=>$tgUserData['last_name']??null,
+    'photo_url'=>$tgUserData['photo_url']??null,
+    'auth_date'=>$tgUserData['auth_date']??time()
 ]);
 
 // Set session
-$_SESSION['uid']  = $user['id'];
+$_SESSION['uid'] = $user['id'];
 $_SESSION['tgid'] = $user['telegram_id'];
 $_SESSION['role'] = $user['role'];
 
